1169444c6c00c09f6bc9fa310c44cf31bc168f5b
Skylix17 <rivaldipebriawan@gmail.com>
2025-11-19 23:59:15 +0800

    membuat karakter mendorong box

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let charX = 256;
+let charX = 250;
 let rectX = 300;
 let rectW = 140;
 let rectH = 140;
@@ -21,10 +21,10 @@ function setup() {
   rectMode(CORNER);
   textSize(14);
 
-  massSlider = createSlider(5, 1000, 500);    // atur massa 5..200
+  massSlider = createSlider(5, 10000, 500);    // atur massa 5..200
   massSlider.position(25, 205);
 
-  forceSlider = createSlider(0, 300, 80);   // atur kekuatan dorong 0..300
+  forceSlider = createSlider(0, 999999, 80);   // atur kekuatan dorong 0..300
   forceSlider.position(25, 235);
 
   frictionSlider = createSlider(0.90, 0.999, 0.90, 0.001);
@@ -41,21 +41,39 @@ function draw() {
   maxForce = forceSlider.value();
   friction = frictionSlider.value();
 
-  // Hitung fisika: a = F / m
-  acceleration = force / mass;
-  velocity += acceleration;
-  rectX += velocity;
-  velocity *= friction;
-
-    // Jika tombol kanan ditekan, beri gaya terus-menerus
+  // Jika tombol kanan ditekan, beri gaya terus-menerus
   if (keyIsDown(RIGHT_ARROW)) {
-    force = maxForce
+    charX += 2;
     pushing = true;
+    force = maxForce
   } else {
-    force = 0;   // Reset gaya setiap frame (kita anggap gaya diterapkan saat tombol ditekan)
     pushing = false;
+    force = 0;  // Reset gaya setiap frame (kita anggap gaya diterapkan saat tombol ditekan)
   }
 
+  let charRight = charX + 40;
+  let boxLeft = rectX;
+
+  let touching = charRight >= boxLeft;
+
+  // Hitung fisika: a = F / m
+  if (touching) {
+    charX = rectX - 40;
+    acceleration = force / mass;
+    velocity += acceleration;
+    rectX += velocity;
+    velocity *= friction;
+
+  } else {
+    acceleration = 0;
+  }
+
+  // batas kanan
+  // cek batas canvas
+  if (rectX + rectW > width) {
+    rectX = width - rectW;
+    velocity = 0;
+  }
   // Karakter sederhana (kotak)
     if (pushing) {
     // pose mendorong: condong ke depan
@@ -74,18 +92,12 @@ function draw() {
   fill(100);
   rect(rectX, height - 200, rectW, rectH);
 
-  // batas kanan
-  // cek batas canvas
-  if (rectX + rectW > width) {
-    rectX = width - rectW;
-    velocity = 0;
-}
-
   // Info
   fill(0);
   text("m / Massa batu: " + mass + "kg", 10, 20);
-  text("F/ Kekuatan dorong/Gaya/Force: " + maxForce + "N", 10, 50);
-  text("Friction/ Medan Gesekan: " + nf(friction,1,3), 300, 50);
+  text("F/ Kekuatan dorong / Gaya / Force: " + maxForce + "N", 10, 50);
+  text("Friction / Medan Gesekan: " + nf(friction,1,3), 300, 50);
   text("a / Akselerasi / Percepatan =" + nf(acceleration,1,3) + "m/s²", 10, 80);
   text("v / Kecepatan gerak benda =" + nf(velocity,1,3)+ "m/s" , 10, 95);
+  
 }
